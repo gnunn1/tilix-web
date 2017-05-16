e7a0b37f545e8754fb389f715a41b043827013c4
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2017-05-16 04:02:08 +0200

    use sass by default, implement gulp to autoprefix, minify css and uglify js, reduce img sizes and few design modifications

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+const gulp = require("gulp")
+const imagemin = require('gulp-imagemin')
+const uglify = require('gulp-uglify')
+const pump = require('pump')
+const concat = require('gulp-concat')
+// coming soon
